05ff3462562ecbf2a97b3e5bc6acbccb3bdf84fa
Nate Johnson <njohnson@107-c02yp55hlvch.lan>
2021-08-25 16:34:32 -0500

    fixed an issue in login-service-test

--- src/login/login-service.test.tsx
+++ src/login/login-service.test.tsx
@@ -2,7 +2,7 @@ import { loginUser } from './login-service';
 import nock from 'nock';
 
 it('should validate user credentials', async () => {
-    const expected = true;
+    const expected = { data: true };
     const inputs = { username: 'nate', password: 'nate' };
 
     nock('http://localhost:3001')
@@ -13,5 +13,6 @@ it('should validate user credentials', async () => {
         .post('/loginUser', inputs)
         .reply(200, expected);
     const actual = await loginUser(inputs);
+    console.log(actual);
     expect(actual).toEqual(expected);
 });
