d9bb0d35ba73178967a3475949b8407a623785c1
Nate Johnson <njohnson@107-c02yp55hlvch.lan>
2021-08-26 10:42:22 -0500

    input fields now properly send their values for verification

--- src/login/Login.test.tsx
+++ src/login/Login.test.tsx
@@ -1,4 +1,5 @@
 import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Login from './Login';
 import { loginUser } from './login-service';
 jest.mock('./login-service');
@@ -29,6 +30,15 @@ it('Should call the login service on button click', async () => {
     const buttonText = 'Login';
     render(<Login />);
     const button = await screen.findByText(buttonText);
+    const usernameField = 'Username:';
+    const usernameInput = await screen.findByLabelText(usernameField);
+    const passwordField = 'Password:';
+    const passwordInput = await screen.findByLabelText(passwordField);
+    const usernameValue = 'John';
+    userEvent.type(usernameInput, usernameValue);
+    const passwordValue = 'smith';
+    userEvent.type(passwordInput, passwordValue);
     fireEvent.click(button);
     expect(mockLogin).toBeCalledTimes(1);
+    expect(mockLogin).toHaveBeenCalledWith({ username: usernameValue, password: passwordValue });
 });
--- src/login/Login.tsx
+++ src/login/Login.tsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import { loginUser } from './login-service';
 
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
     return (
         <>
             <label htmlFor="username">Username:</label>
-            <input id="username"></input>
+            <input id="username" value={username} onChange={(event) => setUsername(event.target.value)}></input>
             <label htmlFor="password">Password:</label>
-            <input id="password"></input>
+            <input id="password" value={password} onChange={(event) => setPassword(event.target.value)}></input>
 
-            <button onClick={() => loginUser({ username: 'nate', password: 'nate' })}>Login</button>
+            <button onClick={() => loginUser({ username: username, password: password })}>Login</button>
         </>
     );
 }
